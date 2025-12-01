e618ad3692ca897f2b2ec379f897e7bc56061ae0
harvestwalukow <harvest0805@gmail.com>
2025-12-01 19:48:23 +0700

    style: data

--- app/page.tsx
+++ app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
         </h2>
         <p className="mt-4 text-gray-300">
           I am a student majoring in Data Science Technology at Airlangga
-          University. Learning and building Data/AI/ML stuffs.
+          University. Learning and building data/AI/ML stuffs.
         </p>
       </section>
 
